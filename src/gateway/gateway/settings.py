200f039080be642ce06d459d8f5d6da33e29ce69
andryushabolkonskiy <seregina200029@bk.ru>
2022-12-22 02:03:34 +0300

    secret_key_-

--- src/gateway/gateway/settings.py
+++ src/gateway/gateway/settings.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import os
 
-SECRET_KEY = os.environ['SECRET_KEY']
+#SECRET_KEY = os.environ['SECRET_KEY']
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
