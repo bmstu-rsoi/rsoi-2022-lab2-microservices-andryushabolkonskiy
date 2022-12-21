1990b6da85e375a4f3bd14e2126f400d24c23f92
andryushabolkonskiy <seregina200029@bk.ru>
2022-12-21 22:57:07 +0300

    gateway added

--- src/gateway/manage.py
+++ src/gateway/manage.py
@@ -4,11 +4,11 @@ import os
 import sys
 from django.core.management.commands.runserver import Command as runserver
 
-runserver.default_port = "8060"
+runserver.default_port = "8080"
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flight.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gateway.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
